2686d3184c99476542f8820b03b87d45f6a97569
Sharath <sharatkm@gmail.com>
2019-11-17 21:42:45 -0600

    contract and truffle migrate

---
+++ contracts/CarMarketPlace.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.8;
+
+contract CarMarketPlace {
+    
+    address payable private owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    struct Car {
+        string vin;
+        string make;
+        string model;
+        uint year;
+        uint price;
+    }
+    
+    mapping(address => Car[]) public sellerCarsMap;
+    
+    modifier validateSellCarRequest(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) {
+        require(bytes(_vin).length > 0, "VIN is invalid !!");
+        require(bytes(_make).length > 0, "Make is invalid !!");
+        require(bytes(_model).length > 0, "Model is invalid !!");
+        require(_year > 0, "Year is invalid !!");
+        require(_price > 0, "Price is invalid !!");
+        _;
+    }
+    
+    event CarOnSale(string vin, string make, string model, uint year, uint price);
+    
+    function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) public validateSellCarRequest(_vin, _make, _model, _year, _price) {
+        // Seller Creates car and puts on sale
+        sellerCarsMap[msg.sender].push(Car(_vin, _make, _model, _year, _price));
+        // Trigger an event 
+        emit CarOnSale(_vin, _make, _model, _year, _price);
+    }
+    
+    function getNumberOfCarsForSale(address _seller) public view returns(uint) {
+        return sellerCarsMap[_seller].length;
+    }
+    
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var CarMarketPlace = artifacts.require("CarMarketPlace.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(CarMarketPlace);
+};
