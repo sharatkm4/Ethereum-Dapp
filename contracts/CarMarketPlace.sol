5067416995c65a020a3cb1371d7ee0741db34778
Sharath <sharatkm@gmail.com>
2019-11-18 07:54:51 -0600

    test case to createCarForSale

--- contracts/CarMarketPlace.sol
+++ contracts/CarMarketPlace.sol
@@ -18,23 +18,31 @@ contract CarMarketPlace {
     
     mapping(address => Car[]) public sellerCarsMap;
     
-    modifier validateSellCarRequest(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) {
+    /*modifier validateSellCarRequest(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) {
         require(bytes(_vin).length > 0, "VIN is invalid !!");
         require(bytes(_make).length > 0, "Make is invalid !!");
         require(bytes(_model).length > 0, "Model is invalid !!");
         require(_year > 0, "Year is invalid !!");
         require(_price > 0, "Price is invalid !!");
         _;
-    }
+    }*/
     
-    event CarOnSale(string vin, string make, string model, uint year, uint price);
+    /*event CarOnSale(string vin, string make, string model, uint year, uint price);*/
     
-    function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) public validateSellCarRequest(_vin, _make, _model, _year, _price) {
+    /*function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) public validateSellCarRequest(_vin, _make, _model, _year, _price) {
         // Seller Creates car and puts on sale
         sellerCarsMap[msg.sender].push(Car(_vin, _make, _model, _year, _price));
         // Trigger an event 
         emit CarOnSale(_vin, _make, _model, _year, _price);
-    }
+    }*/
+	
+	
+	function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) public {
+        // Seller Creates car and puts on sale
+        sellerCarsMap[msg.sender].push(Car(_vin, _make, _model, _year, _price));
+        // Trigger an event 
+        //emit CarOnSale(_vin, _make, _model, _year, _price);
+    }	
     
     function getNumberOfCarsForSale(address _seller) public view returns(uint) {
         return sellerCarsMap[_seller].length;
--- test/CarMarketPlaceTest.js
+++ test/CarMarketPlaceTest.js
@@ -7,16 +7,26 @@ contract("CarMarketPlace", async accounts => {
 	let thirdAccount = accounts[2]; //Seller 2
 	let fourthAccount = accounts[3]; //Buyer 1
 	let fifthAccount = accounts[4]; //Buyer 2
+	
 	let carMarketPlace;
 	
 	beforeEach(async () => {		
 		carMarketPlace = await CarMarketPlace.new();
 	});
 	
+	it("Sets an owner.", async () => {
+		//let carMarketPlace = await CarMarketPlace.deployed();
+		assert.equal(await carMarketPlace.owner.call(), firstAccount);
+	});
 	
 	it("Initial cars for sale should be empty", async () => {
 		assert.equal(await carMarketPlace.getNumberOfCarsForSale.call(secondAccount), 0);
 		assert.equal(await carMarketPlace.getNumberOfCarsForSale.call(thirdAccount), 0);
+	});	
+	
+	it("Should create a car which is sellable", async () => {
+		await carMarketPlace.createCarForSale("V1","Ford","Mustang",2019,100000, {from: secondAccount});
+		assert.equal(await carMarketPlace.getNumberOfCarsForSale.call(secondAccount), 1);
 	});	
 	
 	/*it("Should throw an error when request is empty", async () => {
@@ -28,9 +38,6 @@ contract("CarMarketPlace", async accounts => {
 		}
 	});*/	
 	
-	it("Sets an owner.", async () => {
-		//let carMarketPlace = await CarMarketPlace.deployed();
-		assert.equal(await carMarketPlace.owner.call(), firstAccount);
-	});
+	
 	
 });
