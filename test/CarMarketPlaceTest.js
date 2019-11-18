43b21f67b9a2093b35686b8e596bcec7b975dd5a
Sharath <sharatkm@gmail.com>
2019-11-17 23:12:14 -0600

    test cases

--- test/CarMarketPlaceTest.js
+++ test/CarMarketPlaceTest.js
@@ -3,9 +3,33 @@ const CarMarketPlace = artifacts.require("CarMarketPlace");
 contract("CarMarketPlace", async accounts => {
 	
 	let firstAccount = accounts[0];
+	let secondAccount = accounts[1]; //Seller 1	
+	let thirdAccount = accounts[2]; //Seller 2
+	let fourthAccount = accounts[3]; //Buyer 1
+	let fifthAccount = accounts[4]; //Buyer 2
+	let carMarketPlace;
 	
-	it("Sets an owner", async () => {
-		let carMarketPlace = await CarMarketPlace.deployed();
+	beforeEach(async () => {		
+		carMarketPlace = await CarMarketPlace.new();
+	});
+	
+	
+	it("Initial cars for sale should be empty", async () => {
+		assert.equal(await carMarketPlace.getNumberOfCarsForSale.call(secondAccount), 0);
+		assert.equal(await carMarketPlace.getNumberOfCarsForSale.call(thirdAccount), 0);
+	});	
+	
+	/*it("Should throw an error when request is empty", async () => {
+		try {
+			await carMarketPlace.getNumberOfCarsForSale({from: secondAccount});
+			assert.fail();
+		} catch (err) {
+			assert.ok(/revert/.test(err.message));
+		}
+	});*/	
+	
+	it("Sets an owner.", async () => {
+		//let carMarketPlace = await CarMarketPlace.deployed();
 		assert.equal(await carMarketPlace.owner.call(), firstAccount);
 	});
 	
