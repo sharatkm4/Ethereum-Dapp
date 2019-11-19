fa70e2a22a7a625926b70817ea54891e56f6beb7
Sharath <sharatkm@gmail.com>
2019-11-18 19:42:09 -0600

    UI for dapp

---
+++ src/car-marketplace-server.js
@@ -0,0 +1,56 @@
+// Create the Express app
+const express = require("express");
+const app = express();
+
+// Enable JSON data in the HTTP request body
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// Include HTTP status codes (like 404 Not Found)
+const HttpStatus = require('http-status-codes');
+
+var cors = require('cors');
+app.use(cors());
+
+var axios = require('axios');
+
+// Enable static access to the "/public" folder
+app.use(express.static('../public'));
+
+// The userRepo holds the user and user validation logic
+const userRepo = require('./user-repo');
+
+app.post('/login', (req, res) => {
+    let user = userRepo.find(
+        req.body.username, req.body.password);
+    if (!user)
+        return res.status(HttpStatus.NOT_FOUND).json(
+            {errorMsg: "Invalid username / password"});
+    return res.json({jsonWallet: user.jsonWallet, buyerSellerType: user.buyerSellerType});
+});
+
+app.post('/register', (req, res) => {
+    userRepo.addUser(
+        req.body.username, req.body.password, req.body.jsonWallet, req.body.buyerSellerType,
+        function success(user) {
+            return res.json({"msg": "User successfully registered"});
+        },
+        function error(errorMsg) {
+            return res.status(HttpStatus.CONFLICT).json({errorMsg});
+        }
+    );
+});
+
+var listeningPort = 8888;
+var listeningHost = "localhost";
+
+var server = app.listen(listeningPort, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    if (host == "::") {
+        host = listeningHost;
+    }
+
+    console.log("CarMarketPlace Server listening at http://%s:%s", host, port);
+});
