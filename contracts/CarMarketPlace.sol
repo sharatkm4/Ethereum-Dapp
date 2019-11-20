e6b31a01afbe3d36abd3221ef75263e0961f8892
Sharath <sharatkm@gmail.com>
2019-11-20 11:26:04 -0600

    smart contract refactor and functionality added for buying car

--- contracts/CarMarketPlace.sol
+++ contracts/CarMarketPlace.sol
@@ -1,51 +1,113 @@
-pragma solidity ^0.5.1;
+pragma solidity >=0.4.22 <0.6.0;
+
+import "./Utils.sol";
 
 contract CarMarketPlace {
+    using Utils for *;
     
     address payable public owner;
+	uint public carCount = 0;
     
     constructor() public {
         owner = msg.sender;
     }
     
     struct Car {
+        uint id;
         string vin;
         string make;
         string model;
         uint year;
         uint price;
+        string imageUrl;
+        address payable owner;
+        bool purchased;
     }
     
-    mapping(address => Car[]) public sellerCarsMap;
+    mapping(uint => Car) public carsMap;
     mapping(string => bool) public uniqueVinMap;
     
-    event CarOnSale(string vin, string make, string model, uint year, uint price);
+    event CarOnSale(uint carCount, string vin, string make, string model, uint year, uint price, string ipfsImageUrl, address owner, bool purchased);
     
-	modifier validateSellCarRequest(string memory vin, string memory make, string memory model, uint year, uint price) {
+	modifier validateSellCarRequest(string memory vin, string memory make, string memory model, uint year, uint price, string memory ipfsImageUrl) {
         require(bytes(vin).length > 0, "VIN is invalid !!");
 		require(bytes(make).length > 0, "Make is invalid !!");
 		require(bytes(model).length > 0, "Model is invalid !!");
 		require(year >= 2000, "Year is invalid !!");
 		require(price > 100000, "Price is invalid !!");
+		require(bytes(ipfsImageUrl).length > 0, "Image URL is invalid !!");
         _;
     }
     
     modifier validateUniqueVin(string memory vin) {
-        require(uniqueVinMap[vin] != true, "VIN already exist !!");
+        require(uniqueVinMap[vin] != true, "VIN/Car already exists !!");
         _;
     }
-	
-	function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price) public validateSellCarRequest(_vin, _make, _model, _year, _price) validateUniqueVin(_vin) {
+    
+    function createCarForSale(string memory _vin, string memory _make, string memory _model, uint _year, uint _price, string memory _ipfsImageUrl) public validateSellCarRequest(_vin, _make, _model, _year, _price, _ipfsImageUrl) validateUniqueVin(_vin) {
+	    //Increment car count
+	    carCount++;
         // Seller Creates car and puts on sale
-        sellerCarsMap[msg.sender].push(Car(_vin, _make, _model, _year, _price));
-		//Map used later to check and avoid creating duplicate cars/vins for sale
+        carsMap[carCount] = Car(carCount, _vin, _make, _model, _year, _price, _ipfsImageUrl, msg.sender, false);
+        //Map used later to check and avoid creating duplicate cars/vins for sale
         uniqueVinMap[_vin] = true;
-        // Trigger an event 
-        emit CarOnSale(_vin, _make, _model, _year, _price);
+		//Trigger event when car is created for sale 
+        emit CarOnSale(carCount, _vin, _make, _model, _year, _price, _ipfsImageUrl, msg.sender, false);
     }	
     
-    function getNumberOfCarsForSale(address _seller) public view returns(uint) {
-        return sellerCarsMap[_seller].length;
+    event CarPurchasedFromSeller(uint id, string vin, string make, string model, uint year, uint price, string ipfsImageUrl, address owner, bool purchased);
+    
+    event sellerAndSmartContractBalanceAfterPurchase(uint sellerBalanceBefore, uint amountToSendToSeller, uint sellerBalanceAfter, uint marketPlaceSmartContractBalance);
+    
+    modifier validId(uint id) {
+        require(id > 0 && id <= carCount, "Not a valid ID !!");
+        _;
+    }
+    
+    function buyCarFromSeller(uint _id) public payable validId(_id) {
+        // Retrieve the car 
+        Car memory car = carsMap[_id];
+        // Get the owner/seller
+        address payable seller = car.owner;
+        
+        //Validate buyer is not the same as seller
+        require(msg.sender != seller, "Invalid buyer !! Are you the seller ? ");
+        
+        //Validate buyer has enough balance and price sent in the transaction is equal to or greater than car price set by seller
+        require(msg.sender.balance >= car.price && msg.value >= car.price, "Invalid price sent to buy the car !!");
+        
+        //validate car is not purchased already
+        require(!car.purchased, "Car has already been purchased by a different buyer !!");
+        
+        //Update the car as purchased
+        car.purchased = true;
+        //Buyer will be the new owner
+        car.owner = msg.sender;
+        
+        //re-assign the updated car
+        carsMap[_id] = car;
+        
+        //Pay 95% of the total price (in wei) to seller
+        //Pay 5% of the total price (in wei) to CarMarketPlace smart contract
+        uint totalCarPrice = msg.value;
+        //uint amountToSendToSmartContract = Utils._getPercentageOfTotalAmount(uint(5), uint(100), uint(totalCarPrice));
+        //uint amountToSendToSeller = Utils._safeMathSubtract(totalCarPrice, amountToSendToSmartContract);
+        uint amountToSendToSeller = Utils._getPercentageOfTotalAmount(uint(95), uint(100), uint(totalCarPrice));
+
+        uint sellerBalanceBefore = seller.balance;
+        //Transfer 95% of the price to seller
+        seller.transfer(amountToSendToSeller);
+        uint sellerBalanceAfter = seller.balance;
+        
+        //5% of the price is added to this contract's balance 
+        //We are not explicitly adding the balance since contract's function is payable and it holds all the money sent to it initially. 
+        uint marketPlaceSmartContractBalance = address(this).balance;
+        
+        //Trigger event when car is purchased by buyer
+        emit CarPurchasedFromSeller(_id, car.vin, car.make, car.model, car.year, car.price, car.imageUrl, msg.sender, true);
+        
+        //Trigger event for seller and smartContract balance after car purchase
+        emit sellerAndSmartContractBalanceAfterPurchase(sellerBalanceBefore, amountToSendToSeller, sellerBalanceAfter, marketPlaceSmartContractBalance);
     }
     
 }
---
+++ contracts/Utils.sol
@@ -0,0 +1,67 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+library Utils {
+    
+    // Allow for safely adding two numbers.
+    // Source --> https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol
+    function _safeMathAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "_safeMathAdd : Addition Overflow");
+
+        return c;
+    }
+    
+    // Allows for safely subtracting two numbers.
+    // Source --> https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol
+    function _safeMathSubtract(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "_safeMathSubtract : Subtraction Overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+    
+    // Allows for safely dividing two numbers.
+    // Source --> https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol
+    function _safeMathDivide(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "_safeMathDivide : Denominator is 0"); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        require(a == b * c + a % b, "_safeMathDivide : Division Overflow"); // There is no case in which this doesn't hold
+        return c;
+    }
+    
+    // Allows for safely multiplying two numbers.
+    // Source --> Simple Timed Auction Homeowrk Assignment and https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol
+    function _safeMathMultiply(uint _a, uint _b) internal pure returns (uint) {
+        if (_a == 0 || _b == 0) {
+            return 0;
+        }
+        
+        uint c = _a * _b;
+        
+        require(c / _a == _b, "_safeMathMultiply : Multiplication Overflow");
+        require(c / _b == _a, "_safeMathMultiply : Multiplication Overflow");
+        
+        return c;
+    }
+    
+    // Compares two strings for equality.
+    // Reference --> https://ethereum.stackexchange.com/questions/30912/how-to-compare-strings-in-solidity
+    function _compareStringsEqual(string memory a, string memory b) internal pure returns (bool) {
+        return ( keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
+    }
+    
+    // Allows for safely obtaining the Percentage of a Total Amount.
+    // Source --> https://ethereum.stackexchange.com/questions/36272/get-percentage-of-gas-price-solidity-ethereum
+    function _getPercentageOfTotalAmount(uint256 _numerator, uint256 _denominator, uint256 _totalAmount) internal pure returns (uint256) {
+        uint256 totalNumerator = _safeMathMultiply(_numerator, _totalAmount);
+        return _safeMathDivide(totalNumerator, _denominator);
+    }
+    
+    // Source --> https://ethereum.stackexchange.com/questions/62222/address-payable-type-store-address-and-send-later-using-solidity-0-5-0
+    function _convertAddressToAddressPayable(address _incomingAddress) internal pure returns (address payable) {
+        address payable anAddressPayable = address(uint160(_incomingAddress));
+        return anAddressPayable;
+    }
+    
+}
+
