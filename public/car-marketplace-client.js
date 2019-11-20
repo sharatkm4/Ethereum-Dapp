f2ced8d541ed6c6df0486230ac7f7e1d287c82e3
Sharath <sharatkm@gmail.com>
2019-11-20 13:30:49 -0600

    handling ipfs images on UI

--- public/car-marketplace-client.js
+++ public/car-marketplace-client.js
@@ -1,7 +1,23 @@
 $(document).ready(function () {
     const ethereumProvider = ethers.providers.getDefaultProvider('ropsten');
-    const carMarketplaceContractAddress = "0xc1dcc9e09c708cf915d58b0309e267422b251657";
+    //const carMarketplaceContractAddress = "0xc1dcc9e09c708cf915d58b0309e267422b251657";
+	const carMarketplaceContractAddress = "0xbaaa528c6ba3210ae4c4a7afe041f2545882719c";	
     const carMarketplaceContractABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "buyCarFromSeller",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
 	{
 		"constant": false,
 		"inputs": [
@@ -29,6 +45,11 @@ $(document).ready(function () {
 				"internalType": "uint256",
 				"name": "_price",
 				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_ipfsImageUrl",
+				"type": "string"
 			}
 		],
 		"name": "createCarForSale",
@@ -84,6 +105,12 @@ $(document).ready(function () {
 			},
 			{
 				"indexed": false,
+				"internalType": "string",
+				"name": "ipfsImageUrl",
+				"type": "string"
+			},
+			{
+				"indexed": false,
 				"internalType": "address",
 				"name": "owner",
 				"type": "address"
@@ -96,6 +123,98 @@ $(document).ready(function () {
 			}
 		],
 		"name": "CarOnSale",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "vin",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "make",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "model",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "year",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "ipfsImageUrl",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "purchased",
+				"type": "bool"
+			}
+		],
+		"name": "CarPurchasedFromSeller",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "sellerBalanceBefore",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amountToSendToSeller",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "sellerBalanceAfter",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "marketPlaceSmartContractBalance",
+				"type": "uint256"
+			}
+		],
+		"name": "sellerAndSmartContractBalanceAfterPurchase",
 		"type": "event"
 	},
 	{
@@ -155,6 +274,11 @@ $(document).ready(function () {
 				"type": "uint256"
 			},
 			{
+				"internalType": "string",
+				"name": "imageUrl",
+				"type": "string"
+			},
+			{
 				"internalType": "address payable",
 				"name": "owner",
 				"type": "address"
@@ -178,6 +302,27 @@ $(document).ready(function () {
 				"internalType": "address payable",
 				"name": "",
 				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "uniqueVinMap",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
 			}
 		],
 		"payable": false,
@@ -367,18 +512,24 @@ $(document).ready(function () {
     }
 
     async function loadVotingResults() {
-        try {
-			let candidates = [];
-			let candidatesCount = await carMarketplaceContract.carCount();
-			//alert(candidatesCount);
-			let votingResultsUl = $('#votingResults').empty();
-			for (var i = 1; i <= candidatesCount; i++) {
-				let car = await carMarketplaceContract.carsMap(i);
-				if(!car.purchased) {
-					let li = $('<li>').html(`<b>Vehicle ${i}</b>: <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Vin:</b> ${car.vin} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Make:</b> ${car.make} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Model:</b> ${car.model} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Year:</b> ${car.year} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Price:</b> ${car.price} <br/> <img src='https://ipfs.io/ipfs/QmTFGxtXCWZeTKeJfP7pkNtk6Y1W2Y7ophizwG26ApoBEz' alt='BMW 4 Series' style='width:400px;height:200px;' > <br/>`);
-					li.appendTo(votingResultsUl);
+        try {			
+			let carCount = await carMarketplaceContract.carCount();
+			carCount = parseInt(carCount);
+			console.log('carCount: ', carCount);
+			let carResultsUl = $('#carResults').empty();
+			if(carCount > 0 ){
+				for (var i = 1; i <= carCount; i++) {
+					let car = await carMarketplaceContract.carsMap(i);
+					if(!car.purchased) {
+						let li = $('<li>').html(`<b>Vehicle ${i}</b>: <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Vin:</b> ${car.vin} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Make:</b> ${car.make} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Model:</b> ${car.model} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Year:</b> ${car.year} <br/> &nbsp;&nbsp;&nbsp;&nbsp;<b>Price:</b> ${car.price} <br/> <img src='https://ipfs.io/ipfs/${car.imageUrl}' alt='${car.make} ${car.model}' style='width:400px;height:200px;' > <br/>`);
+						li.appendTo(carResultsUl);
+					}
 				}
-			}			
+			} else {
+				let li = $('<li>').html("No cars are available for sale right now. Please check back later.");
+				li.appendTo(carResultsUl);
+				
+			}
 			
 			
 			/*for (let index = 0; index < candidatesCount; index++) {
@@ -507,7 +658,7 @@ $(document).ready(function () {
 						let ipfsHash = result[0].hash;
 						console.log('Image added to IPFS successfully with hash: ', ipfsHash);
 						
-						carMarketplaceContract.createCarForSale(carSaleVIN, carSaleMake, carSaleModel, carSaleYear, carSalePrice, function (err, txHash) {
+						carMarketplaceContract.createCarForSale(carSaleVIN, carSaleMake, carSaleModel, carSaleYear, carSalePrice, ipfsHash, function (err, txHash) {
 							if (err)
 								return showError("Smart contract call failed when creating your car sale: " + err);
 							console.log(`Your car has been <b>successfully listed</b> for sale. Transaction hash: ${txHash}`);
